9cb46058fd8e3229048aaa0730eae365eb0e8f5f
wolf-22 <kanino@web.de>
2021-07-23 05:15:24 +0200

    Get xxx added

--- Controllers/CityController.cs
+++ Controllers/CityController.cs
@@ -12,4 +12,20 @@ namespace MyWeatherApp.Controllers
     public class CityController : ControllerBase
     {
     }
+
+
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var rng = new Random();
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = rng.Next(-20, 55),
+            Summary = Summaries[rng.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
 }
